848e35fe09bdd93c640a3fc63a23cb93ff256842
Sam Niehaus <sen1019@gmail.com>
2017-11-16 10:42:48 -0500

    Added User Table

--- database/schema.sql
+++ database/schema.sql
@@ -4,6 +4,12 @@
 
 BEGIN;
 
--- CREATE statements go here
+CREATE TABLE app_user (
+    user_id SERIAL,
+    email  VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id)
+);
 
 COMMIT;
