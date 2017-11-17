89bb44a089bda2cdcaebaf25b16a11f89f79562e
Sam Niehaus <sen1019@gmail.com>
2017-11-17 17:09:51 -0500

    Flipped the gardenId with gardenName for Bobby

--- src/main/java/com/techelevator/alpha/model/Garden.java
+++ src/main/java/com/techelevator/alpha/model/Garden.java
@@ -1,19 +1,20 @@
 package com.techelevator.alpha.model;
 
 public class Garden {
-	private String gardenName;
 	private long gardenId;
+	private String gardenName;
 
-	public String getGardenName() {
-		return gardenName;
-	}
-	public void setGardenName(String name) {
-		this.gardenName = name;
-	}
+
 	public long getGardenId() {
 		return gardenId;
 	}
 	public void setGardenId(long gardenId) {
 		this.gardenId = gardenId;
+	}
+	public String getGardenName() {
+		return gardenName;
+	}
+	public void setGardenName(String name) {
+		this.gardenName = name;
 	}
 }
