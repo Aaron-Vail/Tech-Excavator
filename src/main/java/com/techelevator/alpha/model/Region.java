38a7c5dafa5eb86c7d676b8ef27749621cdfdbcd
Richard Hresko <richardhresko@gmail.com>
2017-11-21 14:05:43 -0500

    regiondao returning region as an object

---
+++ src/main/java/com/techelevator/alpha/model/Region.java
@@ -0,0 +1,20 @@
+package com.techelevator.alpha.model;
+
+public class Region {
+
+	private int regionId;
+	private String climate;
+	public int getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
+	}
+	public String getClimate() {
+		return climate;
+	}
+	public void setClimate(String climate) {
+		this.climate = climate;
+	}
+	
+}
