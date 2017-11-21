65b900fed5a26b27e5ed607b6fe6167a1c6b3b95
Sam Niehaus <sen1019@gmail.com>
2017-11-21 10:26:00 -0500

    Parial Plot Controller

--- src/main/java/com/techelevator/alpha/controller/PlotApiController.java
+++ src/main/java/com/techelevator/alpha/controller/PlotApiController.java
@@ -1,17 +1,28 @@
 package com.techelevator.alpha.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.techelevator.alpha.model.AppUser;
 
 //Change later
 @CrossOrigin(origins = "*")
 @RestController 
 public class PlotApiController {
 
-	//@Autowired
-	//PlotDao plotDao;
+	@Autowired
+	PlotDao plotDao;
 	
-	
+	@RequestMapping(path = "/newPlot", method = RequestMethod.POST)
+	public Long newPlot(@ModelAttribute Plot plot, HttpSession session){
+		return plotDao.newPlot(plot, ((AppUser)session.getAttribute("currentUser")).getUserId());
+	}
 	
 	
 }
