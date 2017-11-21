3e1649447597c433e7cca8ea7ba3bdd10dde403c
Sam Niehaus <sen1019@gmail.com>
2017-11-21 11:27:10 -0500

    Actually fixed

--- src/main/java/com/techelevator/alpha/model/JDBCPlotDAO.java
+++ src/main/java/com/techelevator/alpha/model/JDBCPlotDAO.java
@@ -4,7 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JDBCPlotDAO implements PlotDAO {
 	
 	private JdbcTemplate jdbcTemplate;
