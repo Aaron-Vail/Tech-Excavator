1ea636ff2c354941a2acc5b4b91f08aac50a8ef9
Aaron <aaron.vail@yahoo.com>
2017-11-27 18:07:29 -0500

    updated 1-8 sql data

--- bin/database/data.sql
+++ bin/database/data.sql
@@ -12,95 +12,15 @@ INSERT INTO region (region_id, climate) VALUES ('4', 'Warm');
 INSERT INTO region (region_id, climate) VALUES ('5', 'Hot');
 
 --Plant information
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Lycopersicon lycopersicum', 
-					'Tomato', '0.03', '3', 'sun', 'https://static.pexels.com/photos/257794/pexels-photo-257794.jpeg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  Hills: plant 5 to 8 
-					seeds per hill. When plants emerge, cut off weaker plants leaving only 2 to 3 per hill. Allow 3'' to 
-					6'' between hills.  Beds: place hills in beds following spacing for hills above.  Containers: Place 
-					seeds as described above for rows or hills. Use bush-type cucumbers which require less room or grow plants 
-					on cages or on trellises.', '3'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Daucus carota', 'Carrot', 
-					'0.02', '1', 'sun', 'https://static.pexels.com/photos/65174/pexels-photo-65174.jpeg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  
-					Hills: plant 5 to 8 seeds per hill. When plants emerge, cut off weaker plants leaving only 
-					2 to 3 per hill. Allow 3'' to 6'' between hills.  Beds: place hills in beds following spacing 
-					for hills above.  Containers: Place seeds as described above for rows or hills. Use bush-type 
-					cucumbers which require less room or grow plants on cages or on trellises.', '3'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('vulgaris', 'Beet', '0.04', 
-					'1', 'sun', 'https://cdn.pixabay.com/photo/2017/07/08/17/42/beets-2485052_960_720.jpg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4' to 5' apart.  Hills: plant 
-					5 to 8 seeds per hill. When plants emerge, cut off weaker plants leaving only 2 to 3 per hill. 
-					Allow 3'' to 6'' between hills.  Beds: place hills in beds following spacing for hills above.  
-					Containers: Place seeds as described above for rows or hills. Use bush-type cucumbers which require 
-					less room or grow plants on cages or on trellises.', '2'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('napus', 'Turnip', '0.02', 
-					'1', 'sun', 'https://cdn.pixabay.com/photo/2017/04/04/18/00/vegetables-2202506_960_720.jpg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  Hills: plant 
-					5 to 8 seeds per hill. When plants emerge, cut off weaker plants leaving only 2 to 3 per hill. 
-					Allow 3'' to 6'' between hills.  Beds: place hills in beds following spacing for hills above.  
-					Containers: Place seeds as described above for rows or hills. Use bush-type cucumbers which 
-					require less room or grow plants on cages or on trellises.', '2'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Citrullus lanatus', 'Watermelon', '0.07', 
-					'1', 'sun', 'https://cdn.pixabay.com/photo/2017/10/14/15/50/watermelon-2850840_960_720.png', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  Hills: plant 
-					5 to 8 seeds per hill. When plants emerge, cut off weaker plants leaving only 2 to 3 per hill. 
-					Allow 3'' to 6'' between hills.  Beds: place hills in beds following spacing for hills above.  
-					Containers: Place seeds as described above for rows or hills. Use bush-type cucumbers which require 
-					less room or grow plants on cages or on trellises.', '3'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Sativus', 'Cucumber', '0.02', '1', 'sun', 
-					'https://cdn.pixabay.com/photo/2016/11/05/00/01/cucumbers-1799262_960_720.jpg', 'Rows: space plants 
-					placing one seed every 2" to 8" in rows 4'' to 5'' apart.  Hills: plant 5 to 8 seeds per hill. 
-					When plants emerge, cut off weaker plants leaving only 2 to 3 per hill. Allow 3'' to 6'' between hills.  
-					Beds: place hills in beds following spacing for hills above.  Containers: Place seeds as described 
-					above for rows or hills. Use bush-type cucumbers which require less room or grow plants on cages or on trellises.',
-					'3'
-					);
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Vesca', 'Strawberry', '0.04', '1', 'shade', 
-					'https://cdn.pixabay.com/photo/2017/11/05/17/14/strawberry-2921169_960_720.jpg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  
-					Hills: plant 5 to 8 seeds per hill. When plants emerge, cut off weaker plants 
-					leaving only 2 to 3 per hill. Allow 3'' to 6'' between hills.  Beds: place hills 
-					in beds following spacing for hills above.  Containers: Place seeds as described 
-					above for rows or hills. Use bush-type cucumbers which require less room or grow 
-					plants on cages or on trellises.', '3'
-					); 
-					
-INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, 
-					image_link, planting_directions, region) VALUES 
-					('Pepo', 'Pumpkin', '0.03', '6', 'shade', 
-					'https://cdn.pixabay.com/photo/2017/10/29/16/24/autumn-2900167_960_720.jpg', 
-					'Rows: space plants placing one seed every 2" to 8" in rows 4'' to 5'' apart.  Hills: 
-					plant 5 to 8 seeds per hill. When plants emerge, cut off weaker plants leaving only 2 
-					to 3 per hill. Allow 3'' to 6'' between hills.  Beds: place hills in beds following 
-					spacing for hills above.  Containers: Place seeds as described above for rows or hills. 
-					Use bush-type cucumbers which require less room or grow plants on cages or on trellises.', '2'
-					);
-					
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Lycopersicon lycopersicum', 'Tomato', '0.03', '3', 'sun', 'https://static.pexels.com/photos/257794/pexels-photo-257794.jpeg', 'Water semi-frequently', '3');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Daucus carota', 'Carrot', '0.02', '1', 'sun', 'https://static.pexels.com/photos/65174/pexels-photo-65174.jpeg', 'Average Water Needs; Water regularly. Do not overwater', '3');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('vulgaris', 'Beet', '0.04', '1', 'sun', 'https://cdn.pixabay.com/photo/2017/07/08/17/42/beets-2485052_960_720.jpg', 'Direct sow as soon as the ground can be worked', '2');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('napus', 'Turnip', '0.02', '1', 'sun', 'https://cdn.pixabay.com/photo/2017/04/04/18/00/vegetables-2202506_960_720.jpg', 'Average Water Needs; Water regularly. Do not overwater
+Requires consistently moist soil; do not let dry out between waterings', '2');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Citrullus lanatus', 'Watermelon', '0.07', '1', 'sun', 'https://cdn.pixabay.com/photo/2017/10/14/15/50/watermelon-2850840_960_720.png', 'From seed; direct sow after last frost', '3');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Sativus', 'Cucumber', '0.02', '1', 'sun', 'https://cdn.pixabay.com/photo/2016/11/05/00/01/cucumbers-1799262_960_720.jpg', 'Automated testing', '3');
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Vesca', 'Strawberry', '0.04', '1', 'shade', 'https://cdn.pixabay.com/photo/2017/11/05/17/14/strawberry-2921169_960_720.jpg', 'Average Water Needs. Water regularly. Do not overwater', '3'); 
+INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Pepo', 'Pumpkin', '0.03', '6', 'shade', 'https://cdn.pixabay.com/photo/2017/10/29/16/24/autumn-2900167_960_720.jpg', 'From seed; direct sow after last frost', '2');
 INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Brassica eruca', 'Arugula', '0.07', '1', 'sun', 'https://cdn.pixabay.com/photo/2016/05/05/15/11/rocket-1374096_960_720.jpg', 'Average Water Needs. Water regularly. Do not overwater', '3');
 INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Oleracea', 'Kale', '0.08', '2', 'sun', 'https://cdn.pixabay.com/photo/2015/03/26/09/40/plant-690051_960_720.jpg', 'Average Water Needs', '3');
 INSERT INTO plant (scientific_name, common_name, price_per_plant, area_per_plant, desired_light, image_link, planting_directions, region) VALUES ('Oleracea var. botrytis', 'Cauliflower', '0.02', '3', 'sun', 'https://cdn.pixabay.com/photo/2016/10/15/00/50/romanesco-broccoli-1741608_960_720.jpg', 'Average Water Needs', '3');
@@ -116,8 +36,8 @@ INSERT INTO app_user (email, hashed_password, salt) VALUES ('trial@user.edu', 'V
 INSERT INTO garden (garden_name, user_id, plot_json, region) VALUES ('Garden 1', (SELECT user_id FROM app_user WHERE email = 'trial@user.edu'),'{"version":"2.0.0-rc.1","objects":[{"type":"rect","version":"2.0.0-rc.1","originX":"center","originY":"center","left":764,"top":184,"width":200,"height":200,"fill":"#9e6c3a","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":0.9,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","transformMatrix":null,"skewX":0,"skewY":0,"rx":4,"ry":4,"id":1},{"type":"rect","version":"2.0.0-rc.1","originX":"center","originY":"center","left":355.23,"top":187,"width":200,"height":200,"fill":"#9e6c3a","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1.87,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":0.9,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","transformMatrix":null,"skewX":0,"skewY":0,"rx":4,"ry":4,"id":2}],"background":"rgb(249, 252, 252)"}','3');
 INSERT INTO garden (garden_name, user_id, plot_json, region) VALUES ('Garden 2', (SELECT user_id FROM app_user WHERE email = 'trial@user.edu'),'{"version":"2.0.0-rc.1","objects":[{"type":"rect","version":"2.0.0-rc.1","originX":"center","originY":"center","left":633,"top":190,"width":200,"height":200,"fill":"#9e6c3a","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":0.9,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","transformMatrix":null,"skewX":0,"skewY":0,"rx":4,"ry":4,"id":3}],"background":"rgb(249, 252, 252)"}','3');
 INSERT INTO plot (user_id, plot_name, light_level, garden_id, plant_id) VALUES('1','Plot 1','sun','1','5');
-INSERT INTO plot (user_id, plot_name, light_level, garden_id, plant_id) VALUES('1','Plot 2','sun','1','10');
-INSERT INTO plot (user_id, plot_name, light_level, garden_id, plant_id) VALUES('1','Plot 3','sun','2','9');
+INSERT INTO plot (user_id, plot_name, light_level, garden_id, plant_id) VALUES('1','Plot 1','sun','1','10');
+INSERT INTO plot (user_id, plot_name, light_level, garden_id, plant_id) VALUES('1','Plot 1','sun','2','9');
 --Trial Admin Data, password is "Password"
 INSERT INTO app_user (email, hashed_password, salt, admin) VALUES ('trial@admin.edu', 'VDkcZD/a/RrPCsq7BFMEsw==','CBhv/pvtgNgL2YPB46xW2TJ6CwySXJLrweHlzosl/JuelqQo7xHekS1+nzO1f9m5XXfWaPtlo81uYDEK8PQeSPCXRHsGyNNpHVGq+ICHDPyzxzW4/YjFojDzm3R1SOObGgmdNW0QZLSCECuugT//BMoEgEMhx2p4hotiCmAnmqo=',true);
 INSERT INTO garden (garden_name, user_id) VALUES ('Garden 1', (SELECT user_id FROM app_user WHERE email = 'trial@admin.edu'));
---
+++ bin/database/schema.sql
@@ -0,0 +1,57 @@
+-- *************************************************************************************************
+-- This script creates all of the database objects (tables, sequences, etc) for the database
+-- *************************************************************************************************
+
+BEGIN;
+
+CREATE TABLE app_user (
+    user_id SERIAL,
+    email  VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    admin BOOLEAN DEFAULT false,
+    CONSTRAINT pk_user_id PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE region(
+    region_id INT,
+    climate VARCHAR(255),
+    CONSTRAINT pk_region_id PRIMARY KEY (region_id)
+);
+
+CREATE TABLE plant(
+    plant_id SERIAL,
+    scientific_name VARCHAR(255) NOT NULL,
+    common_name VARCHAR(255) NOT NULL,
+    price_per_plant DECIMAL NOT NULL,
+    area_per_plant DECIMAL NOT NULL,
+    desired_light VARCHAR(255) NOT NULL,
+    image_link VARCHAR(255) NOT NULL,
+    planting_directions TEXT NOT NULL,
+    region INT REFERENCES region(region_id),
+    CONSTRAINT pk_plant_id PRIMARY KEY (plant_id)
+);
+
+CREATE TABLE garden(
+    garden_id SERIAL,
+    garden_name VARCHAR(255) NOT NULL,
+    plot_json TEXT DEFAULT 'empty' NOT NULL,
+    user_id INT NOT NULL REFERENCES app_user(user_id),
+    region INT REFERENCES region(region_id),
+    date_deleted DATE,
+    CONSTRAINT pk_garden_id PRIMARY KEY (garden_id)
+);
+
+CREATE TABLE plot(
+    plot_id SERIAL,
+    user_id INT NOT NULL REFERENCES app_user(user_id),
+    plot_name VARCHAR(255),
+    light_level VARCHAR(255),
+    garden_id INT REFERENCES garden(garden_id),
+    plant_id INT REFERENCES plant(plant_id),
+    CONSTRAINT pk_plot_id PRIMARY KEY (plot_id)
+);
+
+
+COMMIT;
