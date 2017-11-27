9c1d88e2b5e6f9a9570ea00547589bd1a50b0809
Richard Hresko <richardhresko@gmail.com>
2017-11-27 12:02:12 -0500

    admin add plant functionality

--- src/main/java/com/techelevator/alpha/model/PlantsDAO.java
+++ src/main/java/com/techelevator/alpha/model/PlantsDAO.java
@@ -1,10 +1,12 @@
 package com.techelevator.alpha.model;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface PlantsDAO {
 
 	public Plants getPlantById(long plantId);
 	public List<Plants> getAllPlants();
+	public void addPlant(String scientificName, String commonName, BigDecimal pricePerPlant, int areaPerPlant, String desiredLight, String imageLink, String plantingDirections, int region);
 
 }
